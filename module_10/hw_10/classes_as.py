714cd81e8c2e11549943d4584e7bb735338e3331
y-hrebonkin <y.hrebonkin@gmail.com>
2023-05-29 23:45:33 +0300

    upd_10

--- module_10/hw_10/classes_as.py
+++ module_10/hw_10/classes_as.py
@@ -1,20 +1,15 @@
-from collections import UserDict
-
 class Field:
     def __init__(self, value=None):
         self.value = value
 
-    def __str__(self):
-        return str(self.value)
-
-    def __repr__(self):
-        return repr(self.value)
 
 class Name(Field):
     pass
 
+
 class Phone(Field):
     pass
+
 
 class Record:
     def __init__(self, name):
@@ -31,35 +26,27 @@ class Record:
         for phone in self.phones:
             if phone.value == old_phone:
                 phone.value = new_phone
-                break
 
-    def __str__(self):
-        return f"Name: {self.name}\nPhones: {', '.join(str(phone) for phone in self.phones)}"
+    def match(self, criteria):
+        if criteria.lower() in self.name.value.lower():
+            return True
+        for phone in self.phones:
+            if criteria in phone.value:
+                return True
+        return False
 
-    def __repr__(self):
-        return f"Record(name={self.name}, phones={self.phones})"
 
-class AddressBook(UserDict):
+class AddressBook:
+    def __init__(self):
+        self.data = {}
+
     def add_record(self, record):
-        self.data[record.name.value] = record
+        if record.name.value not in self.data:
+            self.data[record.name.value] = record
 
-    def __str__(self):
-        return "\n\n".join(str(record) for record in self.data.values())
-
-    def __repr__(self):
-        return repr(self.data)
-
-
-address_book = AddressBook()
-
-record1 = Record("Yevhen Hrebonkin")
-record1.add_phone("0666666114")
-record1.add_phone("0730876937")
-
-record2 = Record("Yurii Haharin")
-record2.add_phone("0675555555")
-
-address_book.add_record(record1)
-address_book.add_record(record2)
-
-print(address_book)
+    def search_records(self, criteria):
+        results = []
+        for record in self.data.values():
+            if record.match(criteria):
+                results.append(record)
+        return results
--- module_10/hw_10/classes_as_plus.py
+++ module_10/hw_10/classes_as_plus.py
@@ -1,27 +1,12 @@
-from collections import UserDict
-
-
-class AddressBook(UserDict):
-    def add_record(self, record):
-        self.data[record.name.value] = record
-
-    def search_records(self, **kwargs):
-        results = []
-        for record in self.data.values():
-            if all(getattr(record, field) == value for field, value in kwargs.items()):
-                results.append(record)
-        return results
-
-    def __str__(self):
-        return "\n".join(str(record) for record in self.data.values())
-
-
 class Field:
     def __init__(self, value=None):
         self.value = value
-
+    
     def __str__(self):
         return str(self.value)
+    
+    def set_value(self, value):
+        self.value = value
 
 
 class Name(Field):
@@ -29,126 +14,152 @@ class Name(Field):
 
 
 class Phone(Field):
-    def __set__(self, instance, value):
-        if not value.isdigit():
-            raise ValueError("Phone number can only contain digits.")
-        self.value = value
+    pass
 
 
 class Record:
     def __init__(self, name):
-        self.name = name
+        self.name = Name(name)
         self.phones = []
-
-    def add_phone(self, phone):
-        self.phones.append(phone)
-
-    def remove_phone(self, phone):
-        self.phones.remove(phone)
-
-    def edit_phone(self, old_phone, new_phone):
-        index = self.phones.index(old_phone)
-        self.phones[index] = new_phone
-
+    
     def __str__(self):
         phones = ", ".join(str(phone) for phone in self.phones)
-        return f"Name: {self.name}\nPhone(s): {phones}"
+        return f"Name: {self.name}, Phones: {phones}"
+    
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+    
+    def remove_phone(self, index):
+        if 0 <= index < len(self.phones):
+            del self.phones[index]
+    
+    def edit_phone(self, index, phone):
+        if 0 <= index < len(self.phones):
+            self.phones[index].set_value(phone)
+
+
+class AddressBook(dict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+    
+    def remove_record(self, name):
+        del self.data[name]
+    
+    def search_records(self, criteria):
+        results = []
+        for record in self.data.values():
+            if criteria.lower() in record.name.value.lower():
+                results.append(record)
+            for phone in record.phones:
+                if criteria in phone.value:
+                    results.append(record)
+        return results
 
 
 def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except (KeyError, ValueError, IndexError) as e:
-            return str(e)
+        except KeyError:
+            return "Invalid command! Please try again."
+        except ValueError:
+            return "Invalid input! Please try again."
+        except IndexError:
+            return "Invalid command! Please try again."
     return inner
 
 
 @input_error
-def command_parser(command):
-    command = command.lower().strip()
-    if command.startswith("add"):
-        return "add", command[4:].strip()
-    elif command.startswith("change"):
-        return "change", command[7:].strip()
-    elif command.startswith("phone"):
-        return "phone", command[6:].strip()
-    elif command.startswith("search"):
-        return "search", command[6:].strip()
-    elif command == "show all":
-        return "show all", ""
-    elif command in ["good bye", "close", "exit"]:
-        return "exit", ""
-    else:
-        return "", ""
+def hello_command():
+    return "How can I help you?"
 
 
 @input_error
-def handle_add(address_book, args):
-    name, phone = args.split(" ")
-    record = Record(Name(name))
-    record.add_phone(Phone(phone))
+def create_contact_command(name, phone, address_book):
+    if name in address_book:
+        return f"Contact '{name}' already exists. Use 'change' command to update the phone number."
+    record = Record(name)
+    record.add_phone(phone)
     address_book.add_record(record)
-    return "Contact added successfully."
+    return f"Contact '{name}' with phone number '{phone}' created."
 
 
 @input_error
-def handle_change(address_book, args):
-    name, phone = args.split(" ")
-    record = address_book.data[name]
-    record.edit_phone(record.phones[0], Phone(phone))
-    return "Phone number changed successfully."
+def change_contact_command(name, phone, address_book):
+    if name not in address_book:
+        return f"Contact '{name}' does not exist. Use 'add' command to create a new contact."
+    record = address_book[name]
+    record.add_phone(phone)
+    return f"Phone number for contact '{name}' changed to '{phone}'."
 
 
 @input_error
-def handle_phone(address_book, args):
-    name = args
-    record = address_book.data[name]
-    return f"Phone number(s) for {name}: {', '.join(str(phone) for phone in record.phones)}"
+def get_phone_command(name, address_book):
+    if name not in address_book:
+        return f"Contact '{name}' does not exist."
+    record = address_book[name]
+    return f"The phone number(s) for contact '{name}' is/are: {', '.join(str(phone) for phone in record.phones)}"
 
 
-@input_error
-def handle_search(address_book, args):
-    kwargs = {}
-    for pair in args.split(","):
-        key, value = pair.strip().split("=")
-        kwargs[key.strip()] = value.strip()
-    results = address_book.search_records(**kwargs)
-    if results:
-        return "\n\n".join(str(record) for record in results)
+def show_all_contacts_command(address_book):
+    if len(address_book) == 0:
+        return "No contacts found."
     else:
-        return "No matching records found."
-
-
-def handle_show_all(address_book, args):
-    return str(address_book)
+        output = "Contacts:\n"
+        for record in address_book.values():
+            output += str(record) + "\n"
+        return output.strip()
 
 
 def main():
     address_book = AddressBook()
-
+    
     while True:
-        command = input("Enter a command: ")
-        action, args = command_parser(command)
+        command = input("Enter a command: ").lower()
 
-        if action == "add":
-            result = handle_add(address_book, args)
-        elif action == "change":
-            result = handle_change(address_book, args)
-        elif action == "phone":
-            result = handle_phone(address_book, args)
-        elif action == "search":
-            result = handle_search(address_book, args)
-        elif action == "show all":
-            result = handle_show_all(address_book, args)
-        elif action == "exit":
+        if command.startswith("add"):
+            _, name, phone = command.split()
+            record = Record(name)
+            record.add_phone(phone)
+            address_book.add_record(record)
+            response = f"Contact '{name}' with phone number '{phone}' created."
+        elif command.startswith("change"):
+            _, name, phone = command.split()
+            if name in address_book:
+                record = address_book[name]
+                record.edit_phone(record.phones[0].value, phone)
+                response = f"Phone number for contact '{name}' changed to '{phone}'."
+            else:
+                response = f"Contact '{name}' does not exist. Use 'add' command to create a new contact."
+        elif command.startswith("phone"):
+            _, name = command.split()
+            if name in address_book:
+                record = address_book[name]
+                phone = record.phones[0].value
+                response = f"The phone number for contact '{name}' is '{phone}'."
+            else:
+                response = f"Contact '{name}' does not exist."
+        elif command == "show all":
+            response = show_all_contacts_command(address_book)
+        elif command in ["good bye", "close", "exit"]:
             print("Good bye!")
             break
         else:
-            result = "Invalid command. Please try again."
+            response = "Invalid command! Please try again."
 
-        print(result)
+        print(response)
 
 
-if __name__ == "__main__":
-    main()
+def show_all_contacts_command(address_book):
+    if len(address_book) == 0:
+        return "No contacts found."
+    else:
+        output = "Contacts:\n"
+        for name, record in address_book.items():
+            phones = ", ".join([phone.value for phone in record.phones])
+            output += f"{name}: {phones}\n"
+        return output.strip()
+
+
+# Run the bot
+main()
